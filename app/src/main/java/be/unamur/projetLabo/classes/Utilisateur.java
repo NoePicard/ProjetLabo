ee324f7f6f119b04d9f93aa6ce296c42216df7f2
Quentin <lombatquentin@live.fr>
2015-05-02 23:33:28 +0200

    Inscription

---
+++ app/src/main/java/be/unamur/projetLabo/classes/Utilisateur.java
@@ -0,0 +1,94 @@
+package be.unamur.projetLabo.classes;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import be.unamur.projetLabo.ProjetLabo;
+
+public class Utilisateur {
+
+
+    private String login;
+    private String password;
+    private boolean fidele, connected, contrated;
+    private int id;
+    //private Voiture voiture;
+    //private Capital capital;
+
+
+
+    public Utilisateur() {
+        this.login = "";
+        this.password = "";
+        this.fidele = false;
+        this.connected = false;
+    }
+    public Utilisateur(String login, String password) {
+        this.login = login;
+        this.password = password;
+        this.fidele = false;
+        this.connected = false;
+    }
+    public Utilisateur(String login, String password, boolean fidele) {
+        this.login = login;
+        this.password = password;
+        this.fidele = fidele;
+        this.fidele = false;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public boolean isFidele() {
+        return fidele;
+    }
+    public void setFidele(boolean fidele) {
+        this.fidele = fidele;
+    }
+    public boolean isContrated() {
+        return contrated;
+    }
+    public void setContrated(boolean contrated) {
+        this.contrated = contrated;
+    }
+    public boolean isConnected() {
+        return connected;
+    }
+    public void connexion() {
+        this.connected = true;
+    }
+    public void deconnexion() {
+        this.connected = false;
+    }
+
+    public boolean hydrate(JSONObject userJSON){
+        try{
+            ProjetLabo.user.setId(userJSON.getInt("id"));
+            ProjetLabo.user.setLogin(userJSON.getString("login"));
+            ProjetLabo.user.setPassword(userJSON.getString("password"));
+            ProjetLabo.user.setFidele(userJSON.getBoolean("fidele"));
+        } catch (JSONException e) {
+            return false;
+        }
+        try{
+        ProjetLabo.user.setContrated(userJSON.getBoolean("contrat"));
+        } catch (JSONException e) {}
+
+        return true;
+    }
+
+}
---
+++ app/src/main/java/be/unamur/projetLabo/classes/bases.java
@@ -0,0 +1,9 @@
+package be.unamur.projetLabo.classes;
+
+public class bases {
+    /*
+        Se retrouve ici toutes les fonctions non lié a un objet et dont l'utilité est commune a tout le projet.
+     */
+
+
+}
